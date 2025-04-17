8c64275fcd97177bad9b1d7e8dd4c15bbe11f562
Salvador Castro <salvacastro06@gmail.com>
2025-04-17 18:19:28 -0300

    chore: update title in header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>Listado</title>
+  <title>CRUD Movies</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
